4d2c53ea49dc55fb0a58ec7de4e8dcd8e000725d
Felipe Genuino <design@felipegenuino.com>
2020-11-17 19:18:30 -0300

    start server

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import express from 'express'
+
+import './database/connection'
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/users ', (request, response ) => {
+    // console.log(request.query)
+    // console.log(request.params)
+    // console.log(request.body)
+    return response.json({message: 'Hello world'})
+})
+
+app.listen(3333)
+
+ 
