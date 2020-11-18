65c8ddb2f8d41c3ff89a0ca438baf5ad44358cde
Felipe Genuino <design@felipegenuino.com>
2020-11-18 02:19:00 -0300

    iniciando sistema de imagens

---
+++ src/database/migrations/1605676070292-create_images.ts
@@ -0,0 +1,43 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class createImages1605676070292 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: 'images',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'integer',
+                    unsigned: true, // coluna n;ao pode ser negativa
+                    isPrimary: true, // indica que é a chave unica de cada usuário
+                    isGenerated: true, // coluna gerada automaticamente
+                    generationStrategy: 'increment' // vai ser auto increment
+                },
+                {
+                    name: 'path',
+                    type: 'varchar'
+                },
+                {
+                    name: 'orphanage_id',
+                    type: 'integer'
+                }
+            ],
+            foreignKeys: [
+                {
+                    name: 'ImageOrphanage',
+                    columnNames: ['orphanage_id'],
+                    referencedTableName: 'orphanages',
+                    referencedColumnNames: ['id'],
+                    onUpdate:'CASCADE',
+                    onDelete: 'CASCADE'
+                }
+            ]
+        }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('images')
+    }
+
+}
