463b611ea613515efd65972c7b182cbd7bf9b218
Felipe Genuino <design@felipegenuino.com>
2020-11-18 01:37:59 -0300

    listando todos os orfanatos

--- src/routes.ts
+++ src/routes.ts
@@ -1,42 +1,12 @@
 import {Router} from 'express'
 
-import {getRepository} from 'typeorm'
-import Orphanage from './models/Orphanage'
+import OrphanagesController from './controllers/OrphanagesControllers'
 
 
 const routes =  Router()
 
-routes.post('/orphanages', async (request, response ) => {
-    //    console.log(request.query)
-    //   console.log(request.params)
-    //   console.log(request.body);
-       const { 
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weekends,
-       } =  request.body;
-
-       const orphanagesRepository = getRepository(Orphanage);
-
-       const orphanage = orphanagesRepository.create({
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weekends,
-       })
-
-    await orphanagesRepository.save(orphanage)
-       
-
-    return response.status(201).json(orphanage)
-})
+routes.get('/orphanages',  OrphanagesController.index)
+routes.post('/orphanages',  OrphanagesController.create)
 
 
 export default routes
